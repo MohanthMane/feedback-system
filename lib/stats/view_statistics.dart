567feb70d1b95933b147904ace52213d9a81862d
Bharadwaj-Dontha <bharadwajdontha00@gmail.com>
2020-04-22 16:50:31 +0530

    Added Widget to work on Statistics

--- lib/stats/view_statistics.dart
+++ lib/stats/view_statistics.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+//import 'package:charts_flutter/flutter.dart';
+
 class Statistics extends StatefulWidget {
   @override
   _StatisticsState createState() => _StatisticsState();
@@ -8,8 +10,22 @@ class Statistics extends StatefulWidget {
 class _StatisticsState extends State<Statistics> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Statistics"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            //Pie Chart Total Stats
+
+            //https://medium.com/flutter/beautiful-animated-charts-for-flutter-164940780b8c
+            //https://google.github.io/charts/flutter/gallery.html
+
+            //Question Wise Stats
+          ],
+        ),
+      ),
     );
   }
 }
